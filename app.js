b4a56e4ff265e25d0ffdb5aac4a3bb06b32836f6
Aishik Barua <aishikbarua07@gmail.com>
2023-12-20 13:39:38 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,57 @@
+//showing navbar
+
+const mobile = document.querySelector('.menu-toggle');
+const mobileLink = document.querySelector('.sidebar');
+
+mobile.addEventListener("click",function(){
+    mobile.classList.toggle("is-active");
+    mobileLink.classList.toggle("active");
+})
+
+//close
+
+mobileLink.addEventListener("click",function(){
+    const menuBars = document.querySelector(".is-active");
+    if (window.innerWidth<=768 && menuBars){
+        mobile.classList.toggle("is-active");
+        mobileLink.classList.toggle("active");
+    }
+})
+
+//move the menu to right and left
+
+var step = 100;
+var stepFilter = 60;
+var scrolling = true;
+
+
+$(".back").bind("click",function(e){
+    e.preventDefault();
+    $(".highlight-wrapper").animate({
+        scrollLeft: "-=" + step + "px"
+    });
+});
+
+$(".next").bind("click",function(e){
+    e.preventDefault();
+    $(".highlight-wrapper").animate({
+        scrollLeft: "+=" + step + "px"
+    });
+});
+
+
+//when click back
+
+$(".back-menus").bind("click",function(e){
+    e.preventDefault();
+    $(".filter-wrapper").animate({
+        scrollLeft: "-=" + stepFilter + "px"
+    });
+});
+
+$(".next-menus").bind("click",function(e){
+    e.preventDefault();
+    $(".filter-wrapper").animate({
+        scrollLeft: "+=" + stepFilter + "px"
+    });
+});
